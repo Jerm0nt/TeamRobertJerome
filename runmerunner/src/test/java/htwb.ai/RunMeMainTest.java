5ed27294dfaddb0e4605b5536dd2923bf55825f4
Jerm0nt <s0566974@htw-berlin.de>
2020-10-20 19:20:45 +0200

    JUnit Tests RunMeMain

--- runmerunner/src/test/java/htwb.ai/RunMeMainTest.java
+++ runmerunner/src/test/java/htwb.ai/RunMeMainTest.java
@@ -1,7 +1,42 @@
 package htwb.ai;
 
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RunMeMainTest {
+
+  RunMeMain runMeMain;
+
+  @Before
+  public void setup(){
+    runMeMain = new RunMeMain();
+  }
+
+  @Test
+  public void checkExistingClass(){
+    String test = "RunMeMock";
+    runMeMain.runMeCheck(test);
+  }
+
+  @Test
+  public void checkNonExistingClass(){
+    String test = "blub";
+    runMeMain.runMeCheck(test);
+  }
+
+  @Test
+  public void checkJavaLangNumberClass(){
+    String test = "java.lang.Number";
+    runMeMain.runMeCheck(test);
+  }
+
+  @Test
+  public void checkJavaIOCloseableClass(){
+    String test = "java.io.Closeable";
+    runMeMain.runMeCheck(test);
+  }
+
 
 }
