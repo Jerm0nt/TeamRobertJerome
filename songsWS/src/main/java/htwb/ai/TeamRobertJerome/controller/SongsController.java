b62a12465b24ba0bd009bf2474601d22b0b62afd
Jerm0nt <s0566974@htw-berlin.de>
2020-11-28 14:27:05 +0100

    add java

---
+++ songsWS/src/main/java/htwb/ai/TeamRobertJerome/controller/SongsController.java
@@ -0,0 +1,48 @@
+package htwb.ai.TeamRobertJerome.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(value="/songs")
+public class SongsController {
+    
+    /*private UserDAO userDAO = new UserDAO();
+
+  //GET http://localhost:8080/authSpring/rest/authNoDI?userId=aschmidt&password=geheim
+    @GetMapping(produces = "text/plain")
+    public ResponseEntity<String> authorizeUser(
+                        @RequestParam("userId") String userId,
+                        @RequestParam("password") String password) 
+                                            throws IOException {            
+        User user = userDAO.getUserByUserId(userId);
+        
+        if (user == null || user.getUserId() == null ||
+            user.getPassword() == null) {
+            return new ResponseEntity<String>("Declined: user nulls??", 
+                    HttpStatus.UNAUTHORIZED);
+        }
+        
+        if (user.getUserId().equals(userId) && user.getPassword().equals(password)) {
+            return new ResponseEntity<String> ("Welcome!", HttpStatus.OK);
+        }
+        return new ResponseEntity<String> ("Not Welcome!", HttpStatus.UNAUTHORIZED);
+    }*/
+	
+    //GET http://localhost:8080/authSpring/rest/authNoDI/1
+    @GetMapping(value="/{id}", produces="text/plain")
+    public ResponseEntity<String> getUser(
+            @PathVariable (value="id") Integer id) throws IOException {
+        /*User user = userDAO.getUserById(id);
+        if (user != null) {
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        }
+        return new ResponseEntity<User>(user, HttpStatus.NOT_FOUND);*/
+      return new ResponseEntity<String>(id.toString(), HttpStatus.OK);
+    }
+
+}
+
