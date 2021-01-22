3ed458cc0672cb7359271181e274c7daa05fcbbf
Robert Dreyer <s0566997@htw-berlin.de>
2021-01-22 10:22:18 +0100

    Update SongIntegrationTest.java

--- songsWS/src/test/java/htwb/ai/TeamRobertJerome/main/SongIntegrationTest.java
+++ songsWS/src/test/java/htwb/ai/TeamRobertJerome/main/SongIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -36,15 +37,40 @@ public class SongIntegrationTest {
     void insertSong() throws Exception {
 
         String song = "{ \"id\": 1, \"title\": \"Nothing else Matters\", \"artist\": \"Metallica\", \"label\": \"Sony\", \"released\": 1999 }";
-        mockMvc.perform(MockMvcRequestBuilders.post("/songs").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+        MvcResult mvcResult =  mockMvc.perform(MockMvcRequestBuilders.post("/songs").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(song)
                 )
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andReturn();
+        String location = mvcResult.getResponse().getHeader("Location");
+        assertTrue(location.equals("http://localhost:8080/songsWS-TeamRobertJerome/rest/songs/1"));
     }
 
     @Test
-    @Order(2)
+    void unauthorized401post() throws Exception {
+        String song = "{ \"id\": 1, \"title\": \"Nothing else Matters\", \"artist\": \"Metallica\", \"label\": \"Sony\", \"released\": 1999 }";
+        mockMvc.perform(MockMvcRequestBuilders.post("/songs").header(HttpHeaders.AUTHORIZATION,"noValidToken")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(song)
+        )
+                .andExpect(status().isUnauthorized());
+    }
+
+
+    @Test
+    void badRequst400post() throws Exception {
+        String song = "{ \"id\": 1, \"title\": \"Nothing else Mattst\"\"Metallica\", \"label\": \"Sony\", \"released\": 1999 }";
+        mockMvc.perform(MockMvcRequestBuilders.post("/songs").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(song)
+        )
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+
     void ok200getSongId1() throws Exception {
 
         mockMvc.perform(
@@ -57,7 +83,30 @@ public class SongIntegrationTest {
     }
 
     @Test
-    @Order(3)
+
+    void unauthorizied401getSongId1() throws Exception {
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/songs/1")
+                        .header(HttpHeaders.AUTHORIZATION,"noValidToken")
+        )
+                .andExpect(status().isUnauthorized());
+
+    }
+
+    @Test
+
+    void notFound404getSongId1() throws Exception {
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/songs/999999")
+                        .header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+        )
+                 .andExpect(status().isNotFound());
+
+    }
+
+    @Test
     void ok200getSongAll() throws Exception {
 
         mockMvc.perform(
@@ -66,6 +115,108 @@ public class SongIntegrationTest {
         )
                 .andExpect(content().string("[{\"id\":1,\"title\":\"Nothing else Matters\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}]"))
                 .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void unauthorized401getSongAll() throws Exception {
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/songs")
+                        .header(HttpHeaders.AUTHORIZATION,"noValidToken")
+        )
+                 .andExpect(status().isUnauthorized());
+
+    }
+
+    @Test
+    void noContent204DeleteSong() throws Exception {
+
+        String song = "{ \"id\": 2, \"title\": \"Nothing else Matters\", \"artist\": \"Metallica\", \"label\": \"Sony\", \"released\": 1999 }";
+       mockMvc.perform(MockMvcRequestBuilders.post("/songs").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(song)
+        )
+                .andExpect(status().isCreated());
+
+       mockMvc.perform(MockMvcRequestBuilders.delete("/songs/2").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isNoContent());
+
+    }
+
+
+    @Test
+    void unauthorized401DeleteSong() throws Exception {
+
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/songs/2").header(HttpHeaders.AUTHORIZATION,"noValidToken")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isUnauthorized());
+
+    }
+
+    @Test
+    void notFound404DeleteSong() throws Exception {
+
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/songs/9999").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    void noContent204PutSong() throws Exception {
+        String song = "{ \"id\": 1, \"title\": \"CHANGED\", \"artist\": \"Metallica\", \"label\": \"Sony\", \"released\": 1999 }";
+        MvcResult mvcResult =  mockMvc.perform(MockMvcRequestBuilders.put("/songs/1").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(song)
+        )
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+
+    }
+
+    @Test
+    void unauthorized401PutSong() throws Exception {
+        String song = "{ \"id\": 1, \"title\": \"CHANGED\", \"artist\": \"Metallica\", \"label\": \"Sony\", \"released\": 1999 }";
+        mockMvc.perform(MockMvcRequestBuilders.put("/songs/1").header(HttpHeaders.AUTHORIZATION,"noValidToken")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(song)
+        )
+                .andExpect(status().isUnauthorized());
+
+
+    }
+
+    @Test
+    void notFound404PutSong() throws Exception {
+        String song = "{ \"id\": 9999, \"title\": \"CHANGED\", \"artist\": \"Metallica\", \"label\": \"Sony\", \"released\": 1999 }";
+        MvcResult mvcResult =  mockMvc.perform(MockMvcRequestBuilders.put("/songs/9999").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(song)
+        )
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+
+    }
+
+    @Test
+    void badRequest400PutSong() throws Exception {
+        String song = "{ \"id\": 1, \"title\": \"CHANGED\", \"artist\": \"Metallica\", \"label\": \"Sony\", \"released\": 1999 }";
+        MvcResult mvcResult =  mockMvc.perform(MockMvcRequestBuilders.put("/songs/2").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(song)
+        )
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
 
     }
 
