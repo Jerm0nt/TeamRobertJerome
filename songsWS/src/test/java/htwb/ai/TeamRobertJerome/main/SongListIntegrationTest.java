af9ea947a87d89ac8f0f3c08b3e7baf2194d8fd1
Robert Dreyer <s0566997@htw-berlin.de>
2021-01-22 11:39:43 +0100

    Update SongListIntegrationTest.java

--- songsWS/src/test/java/htwb/ai/TeamRobertJerome/main/SongListIntegrationTest.java
+++ songsWS/src/test/java/htwb/ai/TeamRobertJerome/main/SongListIntegrationTest.java
@@ -2,14 +2,12 @@ package htwb.ai.TeamRobertJerome.main;
 
 import htwb.ai.TeamRobertJerome.main.repository.SongListRepository;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
@@ -61,33 +59,126 @@ public class SongListIntegrationTest {
     }
 
     @Test
+    @Order(1)
     void postSonglist() throws Exception {
         String songList = "{\"isPrivate\": true, \"name\": \"MaximesPrivateListe\", \"songList\": [ {\"id\":1, \"title\":\"Nothing else Matters\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}, {\"id\": 2, \"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999} ] }";
         MvcResult mvcResult2 =  mockMvc.perform(MockMvcRequestBuilders.post("/songLists").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(songList)
         )
+                .andExpect(content().string("{\"id\":1,\"name\":\"MaximesPrivateListe\",\"songList\":[{\"id\":1,\"title\":\"Nothing else Matters\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999},{\"id\":2,\"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}],\"private\":true,\"isPrivate\":true}"))
+
                 .andExpect(status().isCreated())
                 .andReturn();
 
-        //String location = mvcResult2.getRequest().getHeader("Location");
+        //String location = mvcResult2.getRequest().getContentAsString();
+        //System.out.println(location);
         //assertTrue (location.equals("ttp://localhost:8080/songsWS-TeamRobertJerome/rest/songLists/1"));
 
 
     }
 
     @Test
-    void ok200getSonglist() throws Exception {
-
-        mockMvc.perform(
-                MockMvcRequestBuilders.get("/songLists/songLists?userId=mmuster")
-                        .header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+    @Order(2)
+    void badRequest400postSonglistNotMatchingTitle() throws Exception {
+        String songList = "{\"isPrivate\": true, \"name\": \"MaximesPrivateListe\", \"songList\": [ {\"id\":1, \"title\":\"FALSCHER TITLE\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}, {\"id\": 2, \"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999} ] }";
+        MvcResult mvcResult2 =  mockMvc.perform(MockMvcRequestBuilders.post("/songLists").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(songList)
         )
-                .andExpect(content().string("{\"id\":1,\"title\":\"Nothing else Matters\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}"))
-                .andExpect(status().isOk());
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        //String location = mvcResult2.getRequest().getContentAsString();
+        //System.out.println(location);
+        //assertTrue (location.equals("ttp://localhost:8080/songsWS-TeamRobertJerome/rest/songLists/1"));
+
 
     }
 
+    @Test
+    @Order(2)
+    void notFound404postSonglistWrongSongId() throws Exception {
+        String songList = "{\"isPrivate\": true, \"name\": \"MaximesPrivateListe\", \"songList\": [ {\"id\":99999, \"title\":\"FALSCHER TITLE\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}, {\"id\": 2, \"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999} ] }";
+        MvcResult mvcResult2 =  mockMvc.perform(MockMvcRequestBuilders.post("/songLists").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(songList)
+        )
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        //String location = mvcResult2.getRequest().getContentAsString();
+        //System.out.println(location);
+        //assertTrue (location.equals("ttp://localhost:8080/songsWS-TeamRobertJerome/rest/songLists/1"));
+
+
+    }
+
+    @Test
+    @Order(2)
+    void unauthorized401postSonglistNotValidToken() throws Exception {
+        String songList = "{\"isPrivate\": true, \"name\": \"MaximesPrivateListe\", \"songList\": [ {\"id\":99999, \"title\":\"FALSCHER TITLE\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}, {\"id\": 2, \"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999} ] }";
+        MvcResult mvcResult2 =  mockMvc.perform(MockMvcRequestBuilders.post("/songLists").header(HttpHeaders.AUTHORIZATION,"notValidToken")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(songList)
+        )
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+
+        //String location = mvcResult2.getRequest().getContentAsString();
+        //System.out.println(location);
+        //assertTrue (location.equals("ttp://localhost:8080/songsWS-TeamRobertJerome/rest/songLists/1"));
+
+
+    }
+
+    @Test
+    @Order(2)
+    void ok200getSonglist() throws Exception {
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/songLists?userId=mmuster")
+                        .header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+        )
+                .andExpect(content().string("[{\"id\":1,\"name\":\"MaximesPrivateListe\",\"songList\":[{\"id\":1,\"title\":\"Nothing else Matters\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999},{\"id\":2,\"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}],\"private\":true,\"isPrivate\":true}]"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(3)
+    void ok200getSonglistId() throws Exception {
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/songLists/1")
+                        .header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+        )
+                .andExpect(content().string("{\"id\":1,\"name\":\"MaximesPrivateListe\",\"songList\":[{\"id\":1,\"title\":\"Nothing else Matters\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999},{\"id\":2,\"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}],\"private\":true,\"isPrivate\":true}"))
+                .andExpect(status().isOk());
+    }
+
+
+
+    @Test
+    @Order(4)
+    void noContentDelete() throws Exception {
+
+        String songList = "{\"isPrivate\": true, \"name\": \"MaximesPrivateListe\", \"songList\": [ {\"id\":1, \"title\":\"Nothing else Matters\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}, {\"id\": 2, \"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999} ] }";
+        MvcResult mvcResult2 =  mockMvc.perform(MockMvcRequestBuilders.post("/songLists").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(songList)
+        )
+                .andExpect(content().string("{\"id\":2,\"name\":\"MaximesPrivateListe\",\"songList\":[{\"id\":1,\"title\":\"Nothing else Matters\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999},{\"id\":2,\"title\":\"Enter Sandman\",\"artist\":\"Metallica\",\"label\":\"Sony\",\"released\":1999}],\"private\":true,\"isPrivate\":true}"))
+
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/songLists/2").header(HttpHeaders.AUTHORIZATION,"52b9f172-8845-476d-ae3a-723068f9c3ae")
+
+        )
+                .andExpect(status().isNoContent());
+
+
+    }
 
 
     //... TEST CASES ARE NOT COMPLETE ...//
